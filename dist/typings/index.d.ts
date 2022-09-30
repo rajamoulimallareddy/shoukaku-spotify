391d020361c1819957fcc5aece8063fcecc32157
rajamoulimallareddy <rajamoulimallareddy@gmail.com>
2022-10-01 04:16:20 +0530

    chore: udpate fetchType to string

--- dist/typings/index.d.ts
+++ dist/typings/index.d.ts
@@ -31,7 +31,7 @@ export interface ClientOptions {
      *
      * @param "SCRAPE" | "API"
      */
-    fetchType?: 'API' | 'SCRAPE';
+    fetchType?: string;
 }
 export interface UnresolvedTrack {
     info: {
